afb962e5972d61a38da4085defcd9dba71e977c7
Legendbwoy <90243075+Legendbwoy@users.noreply.github.com>
2024-06-19 03:52:49 +0000

    Updated by CrossUI No-Code App Builder

--- App/js/index.js
+++ App/js/index.js
@@ -13,21 +13,33 @@ xui.Class('App', 'xui.Module',{
             host.ctl_pane10.append(
                 xui.create("xui.UI.Image")
                 .setHost(host,"xui_ui_image15")
-                .setLeft("33.523809523809526em")
-                .setTop("21.333333333333332em")
+                .setLeft("25.142857142857142em")
+                .setTop("17.523809523809526em")
                 .setWidth("8.685714285714285em")
                 .setHeight("7.161904761904762em")
                 .setSrc("{xui.ini.img_pic}")
+                .onDblclick([
+                    {
+                        "desc" : "Action 1",
+                        "type" : "control",
+                        "target" : "xui_ui_image15",
+                        "args" : [ ],
+                        "method" : "fireClickEvent",
+                        "timeout" : 0,
+                        "resetid" : "login.js",
+                        "event" : 1
+                    }
+                ])
             );
             
             host.ctl_pane10.append(
                 xui.create("xui.UI.Label")
                 .setHost(host,"xui_ui_label11")
-                .setLeft("29.714285714285715em")
-                .setTop("31.238095238095237em")
-                .setWidth("18.438095238095237em")
+                .setLeft("18.285714285714285em")
+                .setTop("27.428571428571427em")
+                .setWidth("22.247619047619047em")
                 .setHeight("1.980952380952381em")
-                .setCaption("Click on Logo to Start")
+                .setCaption("Double click on Logo to Start")
                 .setHAlign("center")
                 .setFontSize("18px")
                 .setFontWeight("bold")
