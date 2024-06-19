9272a901e229dc15bb3cfbd75d6d77bd8d3bfc05
Legendbwoy <90243075+Legendbwoy@users.noreply.github.com>
2024-06-19 03:31:28 +0000

    Updated by CrossUI No-Code App Builder

--- App/js/login.js
+++ App/js/login.js
@@ -4,9 +4,63 @@ xui.Class('App.login', 'xui.Module',{
             // [[Code created by CrossUI RAD Studio
             var host=this, children=[], append=function(child){children.push(child.get(0));};
             
-            append((new xui.UI.Pane())
-            .setHost(host,"ctl_pane10")
-            .setDock("fill")
+            append(
+                xui.create("xui.UI.Div")
+                .setHost(host,"ctl_pane10")
+                .setDock("fill")
+            );
+            
+            host.ctl_pane10.append(
+                xui.create("xui.UI.Input")
+                .setHost(host,"xui_ui_input2")
+                .setLeft("19.80952380952381em")
+                .setTop("9.904761904761905em")
+                .setWidth("24.076190476190476em")
+                .setHeight("2.5142857142857142em")
+                .setLabelSize("8em")
+                .setLabelCaption("Username")
+            );
+            
+            host.ctl_pane10.append(
+                xui.create("xui.UI.Input")
+                .setHost(host,"xui_ui_input3")
+                .setLeft("19.80952380952381em")
+                .setTop("15.238095238095237em")
+                .setWidth("24.076190476190476em")
+                .setHeight("2.5142857142857142em")
+                .setLabelSize("8em")
+                .setLabelCaption("Password")
+            );
+            
+            host.ctl_pane10.append(
+                xui.create("xui.UI.Label")
+                .setHost(host,"xui_ui_label3")
+                .setLeft("27.428571428571427em")
+                .setTop("19.047619047619047em")
+                .setWidth("16.152380952380952em")
+                .setHeight("1.2190476190476192em")
+                .setCaption("Don't have an account? Register.")
+            );
+            
+            host.ctl_pane10.append(
+                xui.create("xui.UI.Button")
+                .setHost(host,"xui_ui_button26")
+                .setLeft("29.714285714285715em")
+                .setTop("22.095238095238095em")
+                .setWidth("9.82857142857143em")
+                .setHeight("2.8190476190476192em")
+                .setCaption("Log In")
+            );
+            
+            host.ctl_pane10.append(
+                xui.create("xui.UI.Label")
+                .setHost(host,"xui_ui_label4")
+                .setLeft("26.666666666666668em")
+                .setTop("5.333333333333333em")
+                .setWidth("13.866666666666667em")
+                .setHeight("2.742857142857143em")
+                .setCaption("LOG IN")
+                .setHAlign("center")
             );
             
             return children;
