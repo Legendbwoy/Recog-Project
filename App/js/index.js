df07e409ad1b3504d41585d054f816d63342c131
Legendbwoy <90243075+Legendbwoy@users.noreply.github.com>
2024-06-19 03:14:40 +0000

    Updated by CrossUI No-Code App Builder

--- App/js/index.js
+++ App/js/index.js
@@ -21,73 +21,110 @@ xui.Class('App', 'xui.Module',{
             var host=this, children=[], append=function(child){children.push(child.get(0));};
             
             append(
-                xui.create("xui.UI.ButtonViews")
-                .setHost(host,"xui_ui_buttonviews2")
-                .setItems([
-                    {
-                        "id" : "a",
-                        "caption" : "page1",
-                        "imageClass" : "xui-icon-xui"
-                    },
-                    {
-                        "id" : "b",
-                        "caption" : "page2"
-                    },
-                    {
-                        "id" : "c",
-                        "caption" : "page3"
-                    },
-                    {
-                        "id" : "d",
-                        "caption" : "page4",
-                        "closeBtn" : true,
-                        "optBtn" : "xui-uicmd-opt",
-                        "popBtn" : true
-                    }
-                ])
-                .setLeft("0em")
-                .setTop("0em")
-                .setBarLocation("left")
-                .setBarSize("10.666666666666666em")
-                .setValue("a")
-            );
-            
-            host.xui_ui_buttonviews2.append(
                 xui.create("xui.UI.Block")
-                .setHost(host,"xui_ui_block9")
+                .setHost(host,"xui_ui_block17")
                 .setLeft("0.7619047619047619em")
-                .setTop("0em")
-                .setWidth("41.98095238095238em")
-                .setHeight("19.885714285714286em"),
-                "a"
+                .setTop("0.7619047619047619em")
+                .setWidth("13.028571428571428em")
+                .setHeight("44.266666666666666em")
             );
             
-            host.xui_ui_block9.append(
-                xui.create("xui.UI.Video")
-                .setHost(host,"xui_ui_video1")
+            host.xui_ui_block17.append(
+                xui.create("xui.UI.Button")
+                .setHost(host,"xui_ui_facial")
                 .setLeft("0.6857142857142857em")
                 .setTop("0.6857142857142857em")
-                .setWidth("40.076190476190476em")
-                .setHeight("18.133333333333333em")
+                .setWidth("11.276190476190477em")
+                .setHeight("3.5047619047619047em")
+                .setCaption("FACIAL RECOGNITION")
             );
             
-            host.xui_ui_buttonviews2.append(
-                xui.create("xui.UI.Block")
-                .setHost(host,"xui_ui_block10")
-                .setLeft("0.7619047619047619em")
-                .setTop("20.571428571428573em")
-                .setWidth("41.98095238095238em")
-                .setHeight("7.695238095238095em"),
-                "a"
-            );
-            
-            host.xui_ui_block10.append(
-                xui.create("xui.UI.FormLayout")
-                .setHost(host,"xui_ui_formlayout2")
+            host.xui_ui_block17.append(
+                xui.create("xui.UI.Button")
+                .setHost(host,"xui_ui_collection")
+                .setName("DATA COLLECTION")
                 .setLeft("0.6857142857142857em")
-                .setTop("2.2095238095238097em")
-                .setWidth("40.76190476190476em")
-                .setHeight("4.495238095238095em")
+                .setTop("4.495238095238095em")
+                .setWidth("11.352380952380953em")
+                .setHeight("3.580952380952381em")
+                .setCaption("DATA COLLECTION")
+            );
+            
+            host.xui_ui_block17.append(
+                xui.create("xui.UI.Button")
+                .setHost(host,"xui_ui_settings")
+                .setName("SETTINGS")
+                .setLeft("0.6857142857142857em")
+                .setTop("8.304761904761905em")
+                .setWidth("11.276190476190477em")
+                .setHeight("3.5047619047619047em")
+                .setCaption("SETTINGS")
+            );
+            
+            host.xui_ui_block17.append(
+                xui.create("xui.UI.Button")
+                .setHost(host,"xui_ui_exit")
+                .setName("EXIT")
+                .setLeft("0.7619047619047619em")
+                .setTop("12.19047619047619em")
+                .setWidth("11.2em")
+                .setHeight("3.4285714285714284em")
+                .setCaption("EXIT")
+            );
+            
+            host.xui_ui_block17.append(
+                xui.create("xui.UI.Image")
+                .setHost(host,"xui_ui_image6")
+                .setLeft("0.6857142857142857em")
+                .setTop("17.447619047619046em")
+                .setWidth("11.80952380952381em")
+                .setHeight("11.047619047619047em")
+                .setSrc("{xui.ini.img_pic}")
+            );
+            
+            host.xui_ui_block17.append(
+                xui.create("xui.UI.Block")
+                .setHost(host,"xui_ui_block18")
+                .setLeft("0.6857142857142857em")
+                .setTop("29.714285714285715em")
+                .setWidth("11.504761904761905em")
+                .setHeight("10.666666666666666em")
+            );
+            
+            append(
+                xui.create("xui.UI.Block")
+                .setHost(host,"xui_ui_block19")
+                .setLeft("14.476190476190476em")
+                .setTop("0.7619047619047619em")
+                .setWidth("45.79047619047619em")
+                .setHeight("29.02857142857143em")
+            );
+            
+            host.xui_ui_block19.append(
+                xui.create("xui.UI.Video")
+                .setHost(host,"xui_ui_video3")
+                .setLeft("0.6857142857142857em")
+                .setTop("0.6857142857142857em")
+                .setWidth("43.885714285714286em")
+                .setHeight("27.276190476190475em")
+            );
+            
+            append(
+                xui.create("xui.UI.Block")
+                .setHost(host,"xui_ui_block20")
+                .setLeft("14.476190476190476em")
+                .setTop("30.476190476190474em")
+                .setWidth("45.79047619047619em")
+                .setHeight("13.790476190476191em")
+            );
+            
+            host.xui_ui_block20.append(
+                xui.create("xui.UI.FormLayout")
+                .setHost(host,"xui_ui_formlayout3")
+                .setLeft("0.6857142857142857em")
+                .setTop("1.5238095238095237em")
+                .setWidth("43.733333333333334em")
+                .setHeight("10.285714285714286em")
                 .setLayoutData({
                     "rows" : 5,
                     "cols" : 4,
@@ -109,25 +146,6 @@ xui.Class('App', 'xui.Module',{
                         }
                     }
                 })
-            );
-            
-            append(
-                xui.create("xui.UI.Image")
-                .setHost(host,"xui_ui_image4")
-                .setLeft("1.5238095238095237em")
-                .setTop("12.19047619047619em")
-                .setWidth("7.923809523809524em")
-                .setHeight("7.161904761904762em")
-                .setSrc("{xui.ini.img_pic}")
-            );
-            
-            append(
-                xui.create("xui.UI.Block")
-                .setHost(host,"xui_ui_block8")
-                .setLeft("0.7619047619047619em")
-                .setTop("20.571428571428573em")
-                .setWidth("8.457142857142857em")
-                .setHeight("8.457142857142857em")
             );
             
             return children;
